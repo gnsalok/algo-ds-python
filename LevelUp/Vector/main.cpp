bb1232acb1ff110e3e6cbc66b2e8fde9e05b56d1
Alok Tripathi <gnsalok@gmail.com>
2021-07-29 13:10:25 +0530

    adding LevelUp practice

---
+++ LevelUp/Vector/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+
+    vector<int> arr = {1,2,3,4,5};
+
+    cout<<arr.size() <<endl;
+
+    cout<<arr.capacity() <<endl;
+
+
+
+    return 0;
+}
