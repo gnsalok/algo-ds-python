9bf27f8f01aa105aea636a7efa9dcebc5fd53804
Alok Tripathi <gnsalok@gmail.com>
2021-11-12 18:54:24 +0530

    added levelup

--- LevelUp/Vector/main.cpp
+++ LevelUp/Vector/main.cpp
@@ -4,13 +4,21 @@ using namespace std;
 
 int main(){
 
-    vector<int> arr = {1,2,3,4,5};
+    //vector<int> arr = {1,2,3,4,5};
+
+    vector<int> arr(10,8);
+
+    arr.pop_back();
+
+    arr.push_back(11);
 
     cout<<arr.size() <<endl;
 
     cout<<arr.capacity() <<endl;
 
-
+    for(int i=0; i<arr.size(); i++){
+        cout<< arr[i] <<", ";
+    }
 
     return 0;
 }
