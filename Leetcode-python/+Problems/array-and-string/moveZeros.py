bc455ce37d74fe0621d2560cdb3645c63b3577bb
Alok Tripathi <gnsalok@gmail.com>
2021-11-17 21:31:28 +0530

    added problems

---
+++ Leetcode-python/+Problems/array-and-string/moveZeros.py
@@ -0,0 +1,29 @@
+'''
+Leetcode #283
+Input : 0,1,0,3,12 --> 1,3,12,0,0 (Maintain the relative order)
+
+TC : O(2*N) --> O(N)
+SP : O(1)
+'''
+from typing import List
+
+class Solution:
+    def moveZeros(self, nums: List[int]) -> None:
+        j = 0
+        n = len(arr)
+
+        for num in nums:
+            if(num!=0):
+                nums[j] = num
+                j += 1
+
+        for x in range(j, n):
+            arr[x] = 0
+
+
+arr = [0,1,0,3,12]
+sl = Solution()
+sl.moveZeros(arr)
+print(arr)
+
+
---
+++ test.py
@@ -0,0 +1,8 @@
+dict = {}
+
+num = 2
+
+dict[num] = dict.get(num, 0) + 2
+
+print(dict)
+
