06e3aed5190846ce7f2c78327f9be8197781e070
Alok Tripathi <gnsalok@gmail.com>
2021-11-20 20:10:40 +0530

    python tips

--- Leetcode-python/3_Recursion/test.py
+++ Leetcode-python/3_Recursion/test.py
@@ -6,13 +6,13 @@ TRUST --> Recursion 1.... (n-1) + n
 
 '''
 
-def sum(n):
-    if(n == 0):
-        return 0
 
+
+def sum(n):
+    if(n == 1):
+        return 1
+    
     return sum(n-1) + n
 
 
-
-result = sum(10)
-print(result)
+print(sum(10))
