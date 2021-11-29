8fd7ea3d015314b89817d6f958123865d6af0051
Alok Tripathi <gnsalok@gmail.com>
2021-11-29 13:02:36 +0530

    test

---
+++ algo-ds-java/QuickJava/Test.java
@@ -0,0 +1,17 @@
+public class Test {
+    public static void main(String[] args) {
+
+        // Array
+        // int[] arr = new int[] { 1, 2, 3, 4 };
+        // System.out.print(arr.length);
+
+        // for (int num : arr){
+        //     System.out.println(num);
+        // }
+
+        
+
+
+    }
+
+}
