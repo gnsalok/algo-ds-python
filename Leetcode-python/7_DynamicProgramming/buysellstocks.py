ffdf48d087d16187fa1ecfb02e6818714777e2aa
Alok Tripathi <gnsalok@gmail.com>
2022-01-03 12:36:01 +0530

    dp problems

---
+++ Leetcode-python/7_DynamicProgramming/buysellstocks.py
@@ -0,0 +1,19 @@
+from typing import List 
+
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buyPrice = float("inf") #max int/float value
+        profit = 0
+
+        for i, price in enumerate(prices):
+            if(buyPrice > price):
+                buyPrice = price
+            else:
+                profit = max(profit, price-buyPrice)
+        return profit
+
+prices = [7,1,5,3,6,4]  
+sl = Solution()
+print(sl.maxProfit(prices))  # ans is 5 ; bought price is 1 and sell for 6 > max profile 6 - 1 = 5
--- Leetcode-python/7_DynamicProgramming/rob.py
+++ Leetcode-python/7_DynamicProgramming/rob.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
@@ -6,14 +8,16 @@ class Solution:
             return 0
         
         dp = [0]*n
-        
         dp[0] = nums[0]
-        
+
         for i in range(1,n):
             if(i == 1):
                 dp[i] = max(nums[0], nums[1])
-            
             else:
                 dp[i] = max(dp[i-1], dp[i-2] + nums[i])
-                
+        print(dp)
         return dp[-1]
+
+nums = [1,2,3,1]
+sl = Solution()
+sl.rob(nums)
