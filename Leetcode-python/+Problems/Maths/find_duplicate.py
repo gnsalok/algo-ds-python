f6ee859ddb15c8a65d46c1b9107b44818d181b71
Alok Tripathi <gnsalok@gmail.com>
2022-01-03 10:28:56 +0530

    find duplicate

--- Leetcode-python/+Problems/Maths/find_duplicate.py
+++ Leetcode-python/+Problems/Maths/find_duplicate.py
@@ -4,5 +4,19 @@ Output: 3
 '''
 
 
-arr = [1, 2, 3, 4 ,3]
+def findDup(arr, n):
+
+    for num in arr:
+        absValue = abs(num)  # take abs value
+        if(arr[absValue - 1] < 0): # sub -1 from the actual value 
+            return absValue #if -ve the its first duplicate 
+        arr[absValue - 1] *= -1  # otherwise mark that value as negative to check other iteration 
+  
+    
+
+arr = [1, 2, 2, 3, 4 ,3]
+size = len(arr)
+print(findDup(arr, size))
+
+
 
