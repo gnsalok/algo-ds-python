504597637c7d7173d322b76816ff059d62882169
Alok Tripathi <gnsalok@gmail.com>
2022-01-06 20:55:17 +0530

    reversign list node

--- Leetcode-python/5_LinkedList/reverseLL.py
+++ Leetcode-python/5_LinkedList/reverseLL.py
@@ -5,22 +5,48 @@
 '''
 
 
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+#Definition for singly-linked list.
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
 class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    def reverseList(self, head):
         if(head is None):
             return []
         
         helper = None
+
         while(head is not None):
             nextNode = head.next
             head.next = helper
             helper = head
             head = nextNode 
         return helper
+
+
+head = Node(1)
+sn = Node(3)
+tn = Node(5)
+fon = Node(2)
+fvn = Node(4)
+
+head.next = sn
+sn.next = tn
+tn.next = fon
+fon.next = fvn
+
+sl = Solution()
+
+answer = sl.reverseList(head)
+# print(answer.val)
+
+while(answer is not None):
+    if(answer.val == 1):
+        print(answer.val)
+    else:
+        print(answer.val, end="=>")
+    answer = answer.next
+
             
         
