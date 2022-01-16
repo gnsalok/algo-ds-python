19db613dbb1e98c2cd67c98d57bf81fe433a0f21
Alok Tripathi <gnsalok@gmail.com>
2022-01-16 15:00:52 +0530

    problems

--- Leetcode-python/+Problems/Maths/find_duplicate.py
+++ Leetcode-python/+Problems/Maths/find_duplicate.py
@@ -4,14 +4,26 @@ Output: 3
 '''
 
 
+''''
+This will work only when given arr in 1..n  and you can mutate the array.
+'''
+'''
 def findDup(arr, n):
-
     for num in arr:
         absValue = abs(num)  # take abs value
         if(arr[absValue - 1] < 0): # sub -1 from the actual value 
             return absValue #if -ve the its first duplicate 
         arr[absValue - 1] *= -1  # otherwise mark that value as negative to check other iteration 
-  
+'''
+
+# Will work for every case
+def findDup(arr, n):
+    seen = set()
+    for n in arr:
+        if(n in seen):
+            return n
+        else:
+            seen.add()
     
 
 arr = [1, 2, 2, 3, 4 ,3]
