22bb175aa4d73c3aa8abe580fa1d88889f92d621
Alok Tripathi <gnsalok@gmail.com>
2022-01-22 04:27:52 +0530

    queue & circular queue

---
+++ Leetcode-python/+Problems/Queue/circularQueue.py
@@ -0,0 +1,62 @@
+'''
+Implement a circular queue.
+'''
+
+
+class CirQueue:
+    def __init__(self, cap):
+        self.cap = cap
+        self.storage = [None] * cap
+        self.head = 0
+        self.tail = 0
+        self.size = 0
+
+    def isFull(self):
+        return self.cap == self.size
+
+    def enqueue(self, data):
+        if(self.isFull()):
+            raise Exception("Queue is Full.")
+        self.storage[self.tail] = data
+
+        # self.tail += 1  // regular queue
+
+        # In change in circular queue
+        self.tail = (self.tail + 1) % self.cap
+        self.size += 1
+
+    def dequeue(self):
+        if(self.size == 0):
+            return None
+
+        data = self.storage[self.head]
+        self.storage[self.head] = None
+
+        # circular queue case
+        self.head = (self.head + 1) % self.cap
+        self.size -= 1
+        return data
+
+    def Display(self):
+        if(self.size == 0):
+            return None
+
+        for v in self.storage:
+            print(v, end=" ")
+
+
+if __name__ == "__main__":
+    q = CirQueue(5)
+    q.enqueue(1)
+    q.enqueue(2)
+    q.enqueue(3)
+    q.enqueue(4)
+    q.enqueue(5)
+
+    print("Deque", q.dequeue())
+    print("Deque", q.dequeue())
+
+    q.enqueue(10)
+    q.enqueue(11)
+
+    q.Display()
