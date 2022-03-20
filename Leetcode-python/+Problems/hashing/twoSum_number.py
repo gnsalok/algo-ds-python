a770cf5de30e581a96782471337b364c9c0430fe
Alok Tripathi <gnsalok@gmail.com>
2022-03-20 21:50:13 +0530

    two sum

---
+++ Leetcode-python/+Problems/hashing/twoSum_number.py
@@ -0,0 +1,28 @@
+'''
+Time : O(n)
+Space : O(n)
+'''
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        seen = {}
+
+        for i in range(0, len(nums)):
+            number = nums[i]
+            goal = target - number
+            if(goal in seen):
+                return [seen[goal], number]
+            else:
+                seen[number] = number
+        return -1
+
+     
+            
+if __name__ == "__main__":
+    sl = Solution()
+    arr = [2,7,11,15]
+    target = 9 
+    ans = sl.twoSum(arr, target)  # [2,7]
+    print(ans)
+
--- Leetcode-python/+Problems/hashing/twoSum_number_index.py
+++ Leetcode-python/+Problems/hashing/twoSum_number_index.py
@@ -7,16 +7,17 @@ class Solution(object):
     def twoSum(self, nums, target):
         map = {}
         for i in range(0,len(nums)):
-            goal = target - nums[i]
+            number = nums[i]
+            goal = target - number
             if goal in map:
                 return [map[goal], i]
-            map[nums[i]] = i
+            map[number] = i
             
 if __name__ == "__main__":
     sl = Solution()
     arr = [2,7,11,15]
     target = 9 
-    ans = sl.twoSum(arr, target)
+    ans = sl.twoSum(arr, target) # [0,1]
     print(ans)
     
 
