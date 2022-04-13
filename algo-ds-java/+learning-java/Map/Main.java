fc7ab8eb375f941a37b81efeb277a838c1c1b779
Alok Tripathi <gnsalok@gmail.com>
2022-04-13 17:19:55 +0530

    adding java test

---
+++ algo-ds-java/+learning-java/Map/Main.java
@@ -0,0 +1,54 @@
+package Map;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+/*
+        // All about list
+        List ix = new ArrayList();
+
+        ix.add(12);
+        ix.add(13);
+        ix.add(14);
+
+        System.out.println(ix);
+        System.out.println(ix.get(2));
+        System.out.println(ix.indexOf(13));
+        System.out.println(ix.size());
+ */
+
+        // Map
+/*
+        Map map = new HashMap();
+
+        map.put(2, "Alok");
+
+        System.out.println(map);
+        System.out.println(map.get(2));
+        System.out.println(map.getClass());
+
+        System.out.println(map.values());
+        System.out.println(map.keySet());
+*/
+
+        // It maintains key order in ascending
+//        Map tm = new TreeMap();
+//
+//        tm.put(1,11);
+//        tm.put(3,13);
+//        tm.put(2,10);
+//
+//        System.out.println(tm);
+
+
+        Map mp = new LinkedHashMap();
+
+        mp.put(1,10);
+        mp.put(3,11);
+        mp.put(11,1);
+
+        System.out.println(mp);
+
+    }
+}
+
--- algo-ds-java/Array/Test.java
+++ algo-ds-java/Array/Test.java
@@ -2,7 +2,7 @@ public class Test {
     public static void main(String[] args) {
 
         int[] arr = new int[] { 1, 2, 3, 4 };
-        System.out.print(arr.length);
+        System.out.println(arr.length);
 
         for (int num : arr){
             System.out.println(num);
