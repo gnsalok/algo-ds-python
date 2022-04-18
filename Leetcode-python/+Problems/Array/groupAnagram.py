d755b1bf3dd785cd2655d3576307e50ed3a85c6b
Alok Tripathi <gnsalok@gmail.com>
2022-04-18 09:31:55 +0530

    group anagram

--- Leetcode-python/+Problems/Array/groupAnagram.py
+++ Leetcode-python/+Problems/Array/groupAnagram.py
@@ -2,7 +2,9 @@ class Solution:
    def groupAnagrams(self, strs):
       result = {}
       for i in strs:
+         # sorted returns list of items, need to convert into str
          x = "".join(sorted(i))
+         print(x)
          if x in result:
             result[x].append(i)
          else:
