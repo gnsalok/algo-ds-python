ebf618ad8da4f94264c2708f92b8f8bfe0daaacc
Alok Tripathi <gnsalok@gmail.com>
2022-04-19 22:44:15 +0530

    adding lists of questions

--- Leetcode-python/6_Tree/leetcode/branchSum.py
+++ Leetcode-python/6_Tree/leetcode/branchSum.py
@@ -1,12 +1,9 @@
-'''
-It uses DFS approach and if it finds left and right node as None, it will append the Sum.
-'''
+
 class BinaryTree:
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
-
 
 def branchSums(root):
 	sums = []
@@ -26,5 +23,19 @@ def calculateBranchSum(node, runningSum, sums):
 	calculateBranchSum(node.right, newRunningSum, sums)
 		
 	
-		
-	
+b = BinaryTree(1)
+b.left = BinaryTree(2)
+b.right = BinaryTree(3)
+b.left.left =  BinaryTree(4)
+b.left.right = BinaryTree(5)
+b.right.right = BinaryTree(7)
+b.right.left = BinaryTree(6)
+b.left.right.left = BinaryTree(10)
+b.left.left.left =  BinaryTree(8)
+b.left.left.right =  BinaryTree(9)
+
+
+ans = branchSums(b)
+print(ans)
+
+
