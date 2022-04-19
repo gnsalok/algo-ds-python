7480cea403d9378b357c30792b73aa820ba6719d
Alok Tripathi <gnsalok@gmail.com>
2022-04-19 22:36:57 +0530

    testing

---
+++ Leetcode-python/+Problems/Array/mergeOverlappingIntervals.py
@@ -0,0 +1,25 @@
+'''
+-- Merge Overlapping Intervals 
+  intervals = [[1, 2], [3, 5], [4, 7], [6, 8], [9, 10]]
+  expected = [[1, 2], [3, 8], [9, 10]]
+'''
+
+
+def mergeOverlappingIntervals(intervals):
+
+    
+    # Write your code here.
+    return [[]]
+
+
+if __name__ == "__main__":
+    intervals = [
+        [1, 2],
+        [3, 5],
+        [4, 7],
+        [6, 8],
+        [9, 10]
+    ]
+
+    ans = mergeOverlappingIntervals(intervals)
+    print(ans)
