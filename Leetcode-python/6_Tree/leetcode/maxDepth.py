ffd36261c7ab1ee6ff69e7469783a5b71bb7f78a
Alok Tripathi <gnsalok@gmail.com>
2022-07-31 13:30:07 +0530

    testing

--- Leetcode-python/6_Tree/leetcode/maxDepth.py
+++ Leetcode-python/6_Tree/leetcode/maxDepth.py
@@ -29,9 +29,6 @@ class Solution:
         if(root is None):
             return 0
         
-        if(root.left is None and root.right is None):
-            return 1 
-        
         left = self.maxDepth(root.left)
         right = self.maxDepth(root.right)
         
