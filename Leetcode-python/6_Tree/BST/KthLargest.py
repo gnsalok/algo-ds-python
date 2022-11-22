a90b58140ec3e0dbd6393fbdab873b01c101c146
Alok Tripathi <gnsalok@gmail.com>
2022-11-22 21:53:31 +0530

    leetcode bst

---
+++ Leetcode-python/6_Tree/BST/KthLargest.py
@@ -0,0 +1,45 @@
+# This is an input class. Do not edit.
+class BST:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+
+def inorder(root, nodeValues):
+	if(root is None):
+		return 
+	
+	inorder(root.left,nodeValues)
+	nodeValues.append(root.value)
+	inorder(root.right,nodeValues)
+	
+	
+
+def findKthLargestValueInBst(root, k):
+    nodeValues = []
+    if(root is None):
+        return 
+    inorder(root, nodeValues)
+    print("Node values",nodeValues)
+    return nodeValues[len(nodeValues) - k]
+
+
+
+if __name__ == "__main__":
+    root =  BST(15)
+    root.left = BST(5)
+    root.left.left = BST(2)
+    root.left.left.left = BST(1)
+    root.left.left.right = BST(3)
+    root.left.right = BST(5)
+    root.right = BST(20)
+    root.right.left = BST(17)
+    root.right.right = BST(22)
+    k = 3
+
+    result = findKthLargestValueInBst(root, k)
+    print("Kth largest value in BST : ", result)
+
+	
--- Leetcode-python/7_DynamicProgramming/LCS.py
+++ Leetcode-python/7_DynamicProgramming/LCS.py
@@ -12,6 +12,7 @@ def longestCommonSubsequence(str1, str2):
         for j in range(1, len(str1)+1):
             if (str2[i-1] == str1[j-1]):
                 lcs[i][j] = lcs[i-1][j-1] + [str2[i-1]]
+                print(lcs[i][j], "->", i-1, j-1)
             else:
                 # immidiate top and left max compare based on key
                 lcs[i][j] = max(lcs[i-1][j], lcs[i][j-1], key = len)
