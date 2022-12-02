859d08d1b6b8718d3cc22be9af719f782e3db905
Alok Tripathi <gnsalok@gmail.com>
2022-12-02 21:54:18 +0530

    reversing list

--- Leetcode-python/5_LinkedList/reverseLL.py
+++ Leetcode-python/5_LinkedList/reverseLL.py
@@ -2,8 +2,8 @@
 1,2,3,4,5
 5,4,3,2,1
 
+TC : O(N) == SC
 '''
-
 
 #Definition for singly-linked list.
 class Node:
@@ -12,41 +12,43 @@ class Node:
         self.next = None
 class Solution:
     def reverseList(self, head):
+        ans = []
         if(head is None):
             return []
         
         helper = None
-
         while(head is not None):
             nextNode = head.next
             head.next = helper   # make backward link
             helper = head
             head = nextNode 
-        return helper
 
+        while(helper is not None):
+            ans.append(helper.val)
+            helper = helper.next
+        return ans
 
-head = Node(1)
-sn = Node(3)
-tn = Node(5)
-fon = Node(2)
-fvn = Node(4)
+# Driver
+def main():
+    head = Node(1)
+    sn = Node(3)
+    tn = Node(5)
+    fon = Node(2)
+    fvn = Node(4)
 
-head.next = sn
-sn.next = tn
-tn.next = fon
-fon.next = fvn
+    head.next = sn
+    sn.next = tn
+    tn.next = fon
+    fon.next = fvn
 
-sl = Solution()
+    sl = Solution()
 
-answer = sl.reverseList(head)
-# print(answer.val)
+    answer = sl.reverseList(head)
+    print(answer)
 
-while(answer is not None):
-    if(answer.val == 1):
-        print(answer.val)
-    else:
-        print(answer.val, end="=>")
-    answer = answer.next
+# Main Calling
+if __name__ == "__main__":
+    main()
 
             
         
