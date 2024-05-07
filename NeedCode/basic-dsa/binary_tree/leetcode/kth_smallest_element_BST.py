262f60f20184cde0de15e0d02f91e4cb27f14886
Alok Tripathi <gnsalok@gmail.com>
2024-05-07 15:43:14 +0530

    sol: solving problem

---
+++ NeedCode/basic-dsa/binary_tree/leetcode/kth_smallest_element_BST.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        n = 0
+        stack = []
+        cur = root
+        while cur and stack:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            # when we hit the NULL case, pop from stack
+            # pop meaning visiting that node
+            # - Check if that node is first kth smallest
+            cur = stack.pop()
+            n += 1
+            if n == k:
+                return cur.val
+            cur = cur.right
