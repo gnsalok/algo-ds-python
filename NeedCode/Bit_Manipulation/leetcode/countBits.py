7ad49aff0f037b34dfa1d11b07828c1e4d90da48
Alok Tripathi <gnsalok@gmail.com>
2024-05-13 03:59:53 +0530

    sol: solving problem

---
+++ NeedCode/Bit_Manipulation/leetcode/countBits.py
@@ -0,0 +1,23 @@
+'''
+https://leetcode.com/problems/counting-bits/
+
+
+Draw the Binary Bits from 0 to 8 and see the where offset (Most significant bit) is changing. - Mostly it's power of 2.
+
+
+'''
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        dp = [0] * (n+1)
+        ans = [0]
+        offset = 1
+
+        for i in range(1, n+1):
+            # update offset when it reaches power of 2
+            if offset * 2 == i:
+                offset = i
+            dp[i] = 1 + dp[i - offset]
+        return dp
+
+        
