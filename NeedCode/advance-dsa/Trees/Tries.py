3088bb327a6d43a9f81777354ca50772c31d5487
Alok Tripathi <gnsalok@gmail.com>
2024-05-17 00:30:26 +0530

    sol: solving problem

---
+++ NeedCode/advance-dsa/Trees/Tries.py
@@ -0,0 +1,42 @@
+'''
+Time Complexity :-
+    Insert Word: O(1)
+    Search Word: O(1)
+    Search Prefix: O(1)
+
+'''
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word):
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        #  DONT forget to mark work as TRUE in the end.
+        # WHY? You inserted "apple" doesn't mean you insert "ap" as well.
+        curr.word = True 
+
+    def search(self, word):
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return curr.word
+
+    def startsWith(self, prefix):
+        curr = self.root
+        for c in prefix:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return True
