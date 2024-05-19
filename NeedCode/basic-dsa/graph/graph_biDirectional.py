211b3dec47b84b59b1307912e0204ce0aae03137
Alok Tripathi <gnsalok@gmail.com>
2024-05-19 14:39:00 +0530

    sol: solving problem

---
+++ NeedCode/basic-dsa/graph/graph_biDirectional.py
@@ -0,0 +1,16 @@
+# Given directed edges, build an adjacency list
+edges = [["A", "B"], ["B", "C"], ["B", "E"], ["C", "E"], ["E", "D"]]
+
+adjList = {}
+
+
+
+for src, dst in edges:
+    if src not in adjList:
+        adjList[src] = []
+    if dst not in adjList:
+        adjList[dst] = []
+    adjList[src].append(dst)
+    adjList[dst].append(src)
+
+print(adjList)
