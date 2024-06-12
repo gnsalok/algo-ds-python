d7ecff7cdf6a2fbc5699266d054b40edfa0a3799
Alok Tripathi <gnsalok@gmail.com>
2024-06-12 17:47:48 +0530

    sol: solving problem

---
+++ NeedCode/3-blind-75/linked-list/removeNthFromEnd.py
@@ -0,0 +1,31 @@
+'''
+https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        # dummy node and assign as left ptr so that we will be one node before which we want to delete
+        dummy = ListNode(0, head) 
+        left = dummy
+
+        # make sure distance between left and right pointer in N (ie. 2)
+        right = head
+
+        while n > 0 and right:
+            n -= 1
+            right = right.next
+
+        while right:
+            left = left.next 
+            right = right.next
+
+        left.next = left.next.next
+
+        return dummy.next
+
