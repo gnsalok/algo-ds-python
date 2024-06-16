bc66ca240eefd992dd0583e3c6e17a05e9775a91
Alok Tripathi <gnsalok@gmail.com>
2024-06-16 15:35:45 +0530

    sol: solving problem

--- NeedCode/3-blind-75/DP/1D/climbingStairs.py
+++ NeedCode/3-blind-75/DP/1D/climbingStairs.py
@@ -1,15 +1,27 @@
 '''
+climb stairs 
+- https://leetcode.com/problems/climbing-stairs/
 
 '''
+class Solution:
+    def climbStairs(self, n):
+        # recursive solution
+        # if n == 0:
+        #     return 0
+    
+        # return self.climbStairs(n-1) + 1 # 1 is the count of n which is last step
 
 
-    # one more solution
-    one_step = 1
-    two_step = 2
+        # Using 2 ptr technique 
+        one_step = 1
+        two_step = 2
 
-    for i in range(2, n):
-        temp = two_step
-        next_step = one_step + two_step
-        two_step = next_step
-        one_step = temp
-    return two_step
+        for i in range(2, n):
+            temp = two_step
+            next_step = one_step + two_step
+            one_step = two_step
+            two_step = next_step
+
+        return two_step
+
+
