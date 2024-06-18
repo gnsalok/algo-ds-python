84c1b595b5cedb4b9a99dd9e50d827441d427fb4
Alok Tripathi <gnsalok@gmail.com>
2024-06-18 19:47:06 +0530

    sol: solving problem

---
+++ NeedCode/3-blind-75/1-Famous_Problems/flatten_dictionary.py
@@ -0,0 +1,43 @@
+
+def flatten_dict(d, parent_key='', sep='.'):
+    items = []
+    for k, v in d.items():
+        new_key = f"{parent_key}{sep}{k}" if parent_key else k
+        if isinstance(v, dict):
+            items.extend(flatten_dict(v, new_key, sep=sep).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
+
+
+def isinstance(type1, type2):
+    return type(type1) == type(type2) 
+
+dictionary = {
+            "Key1" : "1",
+            "Key2" : {
+                "a" : "2",
+                "b" : "3",
+                "c" : {
+                    "d" : "3",
+                    "e" : {
+                        "" : "1"
+                    }
+                }
+            }
+        }
+
+result = flatten_dict(dictionary)
+print(result)
+
+'''
+Expected output: 
+
+{
+            "Key1" : "1",
+            "Key2.a" : "2",
+            "Key2.b" : "3",
+            "Key2.c.d" : "3",
+            "Key2.c.e" : "1"
+}
+'''
