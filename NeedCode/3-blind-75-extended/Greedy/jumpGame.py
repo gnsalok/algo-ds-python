7d4f51e327def8b3e986fb76a2fad4807362f431
Alok Tripathi <gnsalok@gmail.com>
2024-06-26 00:52:14 +0530

    sol: solving problem

--- NeedCode/3-blind-75-extended/Greedy/jumpGame.py
+++ NeedCode/3-blind-75-extended/Greedy/jumpGame.py
@@ -3,6 +3,21 @@ https://leetcode.com/problems/jump-game/
 
 TC : O(N)
 
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+
+
+Note:
+If there is 0 in the array you cant reach to the end. Array without 0 is will lead to you result.
+
 '''
 
 class Solution:
