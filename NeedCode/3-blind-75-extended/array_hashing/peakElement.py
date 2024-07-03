9732362356efcda743975aa5891a61b040058671
Alok Tripathi <gnsalok@gmail.com>
2024-07-04 00:06:20 +0530

    sol: solving problem

---
+++ NeedCode/3-blind-75-extended/array_hashing/peakElement.py
@@ -0,0 +1,23 @@
+'''
+https://leetcode.com/problems/find-peak-element/description/
+
+'''
+
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        l, r = 0, len(nums)-1
+
+        while l <= r:
+            mid = l + ((r-l)//2)
+
+
+            # left neighbor is greater
+            if mid > 0 and nums[mid] < nums[mid - 1]:
+                r = mid - 1
+            # right neighbor is greater
+            elif mid < len(nums)-1 and nums[mid] < nums[mid + 1]:
+                l = mid + 1
+            else:
+                return mid
+        
