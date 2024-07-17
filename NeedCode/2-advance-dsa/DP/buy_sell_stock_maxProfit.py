5b98889b9f0dd46ad06c2cc1bd920a64542605e6
Alok Tripathi <gnsalok@gmail.com>
2024-07-17 11:36:06 +0530

    sol: solving problem

--- NeedCode/2-advance-dsa/DP/buy_sell_stock_maxProfit.py
+++ NeedCode/2-advance-dsa/DP/buy_sell_stock_maxProfit.py
@@ -5,6 +5,12 @@ https://leetcode.com/discuss/study-guide/1490172/Dynamic-programming-is-simple
 
 https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/description/
 
+# Recurrence Relation L:
+
+- When we buy a stock, we spend money (+ pay the fee) and therefore decreasing whatever profit we get from the next recursive call
+- When we sell the stock, we get money back => increasing profit
+- When we do nothing, profit doesn't change
+
 '''
 from functools import lru_cache
 from typing import List
@@ -46,7 +52,7 @@ class Solution:
             cache[(pos, bought)] = 0
 
             if not bought:
-                # Buy stock
+                # Buy stock 
                 cache[(pos, bought)] = max(cache[(pos, bought)], dp(pos + 1, True) - prices[pos] - fee)
             else:
                 # Sell stock
