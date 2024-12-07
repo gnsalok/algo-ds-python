296b9fb79d3641a4ada05275cd67c558b94119ba
Alok Tripathi <gnsalok@gmail.com>
2024-12-07 19:25:32 +0530

    sol: update LRU cache comment

--- coding-interview-problems/3-blind-75-extended/1-famous-problems/LRU_cache.py
+++ coding-interview-problems/3-blind-75-extended/1-famous-problems/LRU_cache.py
@@ -13,20 +13,21 @@ class LRUCache:
         self.left, self.right = Node(0, 0), Node(0, 0)  
         self.left.next, self.right.prev = self.right, self.left  # doubly linked list pointing to each other
 
-    # remove node from doubly linked list
+    # remove node from doubly linked list (For DLL operation)
     def remove(self, node):
         prev, nxt = node.prev, node.next 
         prev.next = nxt
         nxt.prev = prev
 
-    # insert node at right (most recently used)
+    # insert node at right (most recently used) (; for DLL operation)
     def insert(self, node):
         prev, nxt = self.right.prev, self.right
         prev.next = node 
         node.prev = prev 
         node.next = nxt
         nxt.prev = node
-
+    
+    # GET will get the element and update the position of node with REMOVE and RE-Insert
     def get(self, key: int) -> int:
         if key in self.cache:
             # remove key from cache and insert to right ; so that we know it's most recently used
@@ -35,6 +36,7 @@ class LRUCache:
             return self.cache[key].val
         return -1
 
+    # PUT will store the value if not already present AND if capacity is reached then it will evict from the cache
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.remove(self.cache[key])
