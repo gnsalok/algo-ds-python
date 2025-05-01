9ce2c6f7afca34d852d653bf3587b146ac053068
Alok Tripathi <gnsalok@gmail.com>
2025-05-01 21:11:56 +0530

    solving problem

--- coding-interview-problems/3-blind-75-extended/two-pointer/trapping-rain-water.py
+++ coding-interview-problems/3-blind-75-extended/two-pointer/trapping-rain-water.py
@@ -3,10 +3,6 @@
 https://leetcode.com/problems/trapping-rain-water/
 
 '''
-
-
-
-
 class Solution:
     def trap(self, height: List[int]) -> int:
         if not height:
@@ -36,3 +32,40 @@ class Solution:
                 totalWater += maxR - height[r]
 
         return totalWater
+
+
+'''
+Other solution with extra memory
+'''
+
+def trap(h):
+    
+    totalWater = 0
+
+    maxLArr = [0]
+    maxRArr = [0]
+
+    maxL = 0
+    maxR = h[-1]
+
+    # calculate max left and store
+    for i in range(1, len(h)):
+        maxL = max(maxL, h[i-1])
+        maxLArr.append(maxL)
+    
+    # calculate max right and store 
+    for i in range(len(h)-2,-1,-1):
+        maxR = max(maxR,h[i+1])
+        maxRArr.insert(0, maxR)
+
+
+    # Now compare both array with formula : min(l, r) - h[i]
+    for i, (x, y) in enumerate(zip(maxLArr, maxRArr)):
+        if min(x, y) - h[i] >=0:
+            totalWater += min(x, y) - h[i]
+
+    print(totalWater)
+
+
+height = [0,1,0,2,1,0,1,3,2,1,2,1]
+trap(height)
